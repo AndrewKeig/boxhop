3530bffdeb43edf449fcfc9c8f069e2f7c2bd297
andrewkeig <andrew.keig@gmail.com>
2011-12-30 18:04:20 +0000

    0.3

---
+++ models/user.js
@@ -0,0 +1,30 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var media = new Schema({
+    imageUrl     : String,
+    videoUrl     : String
+});
+
+var channel = new Schema({
+    name     : String,
+    feed     : [media]
+});
+
+var user = new Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    channels: [channel]
+});
+
+
+//user_schema.virtual('id')
+//    .get(function() {
+//      return this._id.toHexString();
+//    });
+
+mongoose.model('User', user);
+mongoose.model('Channel', channel);
+mongoose.model('Media', media);
+module.exports = mongoose;
